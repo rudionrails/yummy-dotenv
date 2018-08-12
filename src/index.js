2c56949b8cb08c5640386e62af4ef0132ab07165
Rudolf Schmidt <me@rudionrails.com>
2018-08-12 13:09:54 +0200

    Refactors dotenv parsing

--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
-
-function parse(context, file) {
-  const filename = path.resolve(context, file);
-
-  if (!fs.existsSync(filename)) return {};
-  return dotenv.parse(fs.readFileSync(filename));
-}
-
 function config({
   context = path.resolve(process.cwd()),
+  env = process.env.NODE_ENV || 'development',
   system = true,
   files = [
     '.env',
@@ -21,50 +13,46 @@ function config({
     `.env.${NODE_ENV}.local`,
   ],
 }) {
-  const env = {};
+  const exists = file => fs.existsSync(path.resolve(context, file));
+  const parse => file => exists(file) ? dotenv.parse(fs.readFileSync(path.resolve(context, file))) : {};
+  const mergeFile => (object, file) => Object.assign({}, object, parse(file));
+  const onlyKeys = (object, file) => Object.keys(parse(file)).reduce(
+    (acc, key) => Object.assign({}, acc, { [key]: acc[key] }),
+    object,
+  );
 
-  // load the .env.schema
-  //
-  // doing this, nullifies all the values
-  Object.keys(parse(context, '.env.schema')).forEach((key) => {
-    env[key] = null;
-  });
+  return [
+    // load the .env.defaults
+    //
+    // all parsed keys will be assigned, e.g.
+    //    FOO=bar
+    //    # => { FOO: 'bar' }
+    //
+    //    FOO=1
+    //    # => { FOO: '1' }
+    //
+    //    FOO=
+    //    # => { FOO: '' }
+    object => mergeFile(object, '.env.defaults'),
 
-  // load the .env.defaults
-  //
-  // all parsed keys will be assigned, e.g.
-  //    FOO=bar
-  //    # => { FOO: 'bar'
-  //
-  //    FOO=1
-  //    # => { FOO: '1' }
-  //
-  //    FOO=
-  //    # => { FOO: '' }
-  Object.entries(parse(context, '.env.defaults')).forEach(([key, value]) => {
-    env[key] = value;
-  });
+    // load the vars from the files
+    //
+    // if not specifically defined, the default is:
+    //    .env
+    //    .env.local
+    //    .env.development
+    //    .env.development.local
+    object => files.reduce(mergeFile, object),
 
-  // load the vars from the files
-  //
-  // if not specifically defined, the default is:
-  //    .env
-  //    .env.local
-  //    .env.development
-  //    .env.development.local
-  files.map(file => Object.assign(env, parse(context, file)));
+    // when configured (default: true), system vars take precedence
+    //
+    // unlike values taken form the .env.schema, those will be set
+    // to '' (empty string) and not null.
+    object => system ? Object.assign({}, object, process.env) : object,
 
-  // when allowed (default: true), system vars take precedence
-  //
-  // unlike values taken form the .env.schema, those will be set
-  // to '' (empty string) and not null.
-  if (system) {
-    Object.keys(env).forEach((key) => {
-      env[key] = process.env[key] || env[key];
-    });
-  }
-
-  return env;
+    // when configured (default: true), reduce thea vars to the schema
+    object => exists('.env.schema') ? onlyKeys(object, '.env.schema'): object,
+  ].reduce((acc, fn) => fn(acc), {});
 }
 
 export default {
