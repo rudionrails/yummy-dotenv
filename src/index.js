2673343c7b25a4532edca676c0b4ad8706f5e03d
Rudolf Schmidt <me@rudionrails.com>
2018-08-19 17:07:04 +0200

    More refactoring

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ const dotenv = require('dotenv');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
+const pipe = (...list) => object => list.reduce((acc, fn) => fn(acc), object);
+const map = (list, fn) => list.map(fn);
+const when = (predicate, fn) => object => predicate ? fn(object) : object;
+const keys = env => Object.keys(env);
+
+const assign = env => object => Object.assign({}, object, env);
+const only = env => object => Object.keys(object).reduce(
+  (acc, key) => assign({ [key]: env[key] || acc[key] })(acc),
+  object,
+);
+
 function config({
   context = path.resolve(process.cwd()),
   defaults = '.env.defaults',
@@ -16,28 +27,22 @@ function config({
     `.env.${NODE_ENV}.local`,
   ],
 } = {}) {
-  const parse = file => fs.existsSync(path.resolve(context, file))
-    ? dotenv.parse(fs.readFileSync(path.resolve(context, file)))
-    : {};
+  const parse = file => object => {
+    const filename = path.resolve(context, file);
+    const env = fs.existsSync(filename)
+      ? dotenv.parse(fs.readFileSync(path.resolve(context, file)))
+      : {};
 
-  const pick = file => object => Object.keys(parse(file)).reduce(
-    (acc, key) => Object.assign({}, acc, { [key]: object[key] }),
+    return assign(env)(object);
+  };
+  const pick = file => object => Object.keys(parse(file)({})).reduce(
+    (acc, key) => assign({ [key]: object[key] })(acc),
     {},
   );
 
-  const only = env => object => Object.keys(object).reduce(
-    (acc, key) => Object.assign({}, acc, { [key]: env[key] || acc[key] }),
-    object,
-  );
-
-  const pipe = (...list) => object => list.reduce((acc, fn) => fn(acc), object);
-  const when = (predicate, fn) => object => predicate ? fn(object) : object;
-  const assign = file => object => Object.assign({}, object, parse(file));
-  const map = files => object => files.reduce((acc, file) => assign(file)(acc), object);
-
   return pipe(
-    when(defaults, assign(defaults)),
-    map(files),
+    when(defaults, parse(defaults)),
+    pipe(...map(files, parse)),
     when(schema, pick(schema)),
     when(system, only(process.env)),
   )({});
